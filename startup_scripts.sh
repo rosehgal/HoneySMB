019eac4b5eff0f16f56bb202712fdee0ae4a0a96
root <root@smb.localdomain>
2017-04-13 05:29:23 +0000

    modified for errors

--- startup_scripts.sh
+++ startup_scripts.sh
@@ -1,2 +1,3 @@
- #!/bin/bash
-tcpdump -p -f "port 445 or port 139" -w /dev/stdout | ssh -i /home/smb/smbDockerKeys -o StrictHostKeyChecking=no smbuser@`/sbin/ip route|awk '/default/ { print $3 }'` "cat - > /home/smbuser/smbDocker_`date +%Y-%m-%d`.pcap"
+#!/bin/bash
+python /home/smb/smbserver.py &  
+tcpdump -p -f "port 445 or port 139" -w /home/smb/logs/"smblog.pcap"

df140c98241dcd7fb5842007c147baf26f50c4ef
Nishitm@iitk.ac.in <Nishitm@iitk.ac.in>
2017-04-13 11:37:36 +0530

    remove quotes

--- startup_scripts.sh
+++ startup_scripts.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 python /home/smb/smbserver.py &  
-tcpdump -p -f "port 445 or port 139" -w /home/smb/logs/"smblog.pcap"
+tcpdump -p -f "port 445 or port 139" -w /home/smb/logs/smblog.pcap
